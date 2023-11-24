4f6d83e5239d6f41fb21fa57f378354dca54cb93
trakt <trakt@DESKTOP-UDGVRCB>
2023-11-24 12:06:46 +0300

    Добавьте файлы проекта.

---
+++ SeaFight/Start.cpp
@@ -0,0 +1,16 @@
+﻿#include "Main.h"
+#include "Windows.h"
+#include <ctime>
+#include "SeaFight.h"
+#include "Menu.h"
+
+int main()   // Главная функция запуска
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	srand(time(0));
+	options();
+	menu();
+
+	Sleep(INFINITE);   // Приостановка выполнения программы до закрытия вручную
+}
